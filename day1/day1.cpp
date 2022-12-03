524e22b73b5ca6337a9cffe56275e4f4a7c37491
nnorton1207 <noahnorton1207@gmail.com>
2022-12-03 12:20:22 -0500

    not fully working yet, working towards a functional solution (even though i got the answer from it)

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -4,18 +4,40 @@
 using namespace std;
 
 int main (int argc, char* argv[]){
-    int previous_elf = 0, current_elf = 0;
-    string test;
+    int highest_total = 0, current_total = 0;
+    string current_num;
 
-    getline(cin,test);
+    while(getline(cin, current_num)){
+        // get first line
+        if (cin.eof())
+            break;
 
-    while(test != ""){
-        current_elf += stoi(test);
-        getline(cin,test);
+    /**
+     * keep a list of elf items
+     * let max = 0;
+     * for every line of stdin {
+     *   is line == ""? {
+     *     compute the sum
+     *     empty the list
+     * 
+     *     is sum > max? then max = sum
+     *   }
+     *   else add the line to a list of elves
+     * }
+     * 
+     * print(max) --> that's the answer
+    */
+
+        // while we have a list of numbers, add 'em up! stop when you hit a whitespace line.
+        while(current_num != ""){
+            current_total += stoi(current_num);
+            getline(cin,current_num);
+        }
+
+        if(current_total > highest_total)
+            highest_total = current_total;
+
+        current_total = 0;
     }
-
-    cout << current_elf << endl;
-
-    // cycle thru input list, compare current elf calorie total with the previous and replace highest calorie count if prev < current until eof
-
+    cout << highest_total << endl;
 }
