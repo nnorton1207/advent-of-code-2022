abf4520932eeec2378d598a7060f914717f02af7
Noah Norton <noahnorton1207@gmail.com>
2022-12-07 13:41:50 -0500

    reowrked solution, works properly now~

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -4,40 +4,35 @@
 using namespace std;
 
 int main (int argc, char* argv[]){
-    int highest_total = 0, current_total = 0;
-    string current_num;
-
-    while(getline(cin, current_num)){
-        // get first line
-        if (cin.eof())
-            break;
-
+    int list = 0, sum = 0, max = 0;
+    string line;
     /**
-     * keep a list of elf items
-     * let max = 0;
-     * for every line of stdin {
-     *   is line == ""? {
-     *     compute the sum
-     *     empty the list
-     * 
-     *     is sum > max? then max = sum
-     *   }
-     *   else add the line to a list of elves
-     * }
-     * 
-     * print(max) --> that's the answer
-    */
+    * keep a list of elf items
+    * let max = 0;
+    * for every line of stdin {
+    *   is line == ""? {
+    *     compute the sum
+    *     empty the list
+    * 
+    *     is sum > max? then max = sum
+    *   }
+    *   else add the line to a list of elves
+    * }
+    * 
+    * print(max) --> that's the answer
+    **/
 
-        // while we have a list of numbers, add 'em up! stop when you hit a whitespace line.
-        while(current_num != ""){
-            current_total += stoi(current_num);
-            getline(cin,current_num);
+    while(!cin.eof()){
+        getline(cin, line);
+        if(line == ""){
+            sum = list;
+            list = 0;
+            
+            if(sum > max)
+                max = sum;
+        } else {
+            list += stoi(line);
         }
-
-        if(current_total > highest_total)
-            highest_total = current_total;
-
-        current_total = 0;
     }
-    cout << highest_total << endl;
+    cout << max << endl;
 }
